389426c3a46b8e19db1ddbb63d18553dd7ec4860
andersenFuGui <z8817176@163.com>
2017-12-10 16:59:15 +0800

    webpack-bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlplugin = require('html-webpack-plugin');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+const cleaner = require('clean-webpack-plugin');
+var bootstrapEntryPoints = require('./webpack.bootstrap.config');
+var bootstrapConfig = 'isProt' ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
+
+// const glob = require('glob');
+module.exports = {
+  entry: {
+    index: './src/js/index.js',
+    bootstrap: bootstrapConfig
+
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'js/[name].js',
+    publicPath: 'http://localhost:8080/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.art$/,
+        use: [{
+          loader: 'art-template-loader'
+        }]
+      },
+      {
+        test: /\.scss$/,
+        use: extractTextPlugin.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: 'postcss-loader'
+          }, {
+            loader: "sass-loader"
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 500,
+            outputPath: 'images/',
+          }
+        }]
+      },
+      {
+        test: /\.(jsx|js)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "es2015", "react"
+            ]
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(woff2?|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options:{
+            limit:10000,
+            outputPath: 'fonts/',
+          }
+        }]
+      },
+      { test: /\.(ttf|eot)$/, 
+        use: [{
+          loader: 'url-loader',
+          options:{
+            limit:10000,
+            outputPath: 'fonts/',
+          }
+        }] },
+      { test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports-loader?jQuery=jquery' }
+    ]
+  },
+  plugins: [
+    new cleaner('dist'),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.$': 'jquery',
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['jquery'],
+      filename: 'assets/js/[name].js',
+      minChunks: 2
+    }),
+    new extractTextPlugin('css/[name].css'),
+    new htmlplugin({
+      template: './src/index.art'
+    }),
+
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    host: 'localhost',
+    port: 8080, // 默认8080
+    compress: true
+  }
+}
